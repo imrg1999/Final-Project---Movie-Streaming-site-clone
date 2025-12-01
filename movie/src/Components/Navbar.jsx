fd534f1a2e5c75410944ce35986170b2b9b6bdbe
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-12-02 03:10:45 +0530

    styling updated

--- movie/src/Components/Navbar.jsx
+++ movie/src/Components/Navbar.jsx
@@ -6,18 +6,19 @@ const Navbar = () => {
     h-20 text-sm md:text-[15px] font-medium text-nowrap'>
         <label> Logo </label>
         <ul className='hidden xl:flex gap-6'>
-            <li>Home</li>
-            <li>TV Shows</li>
-            <li>Movies</li>
-            <li>Anime</li>
-            <li>Games</li>
-            <li>New & Popular</li>
-            <li>Upcoming</li>
+            <li className='cursor-pointer hover:text-[#e50914]'>Home</li>
+            <li className='cursor-pointer hover:text-[#e50914]'>TV Shows</li>
+            <li className='cursor-pointer hover:text-[#e50914]'>Movies</li>
+            <li className='cursor-pointer hover:text-[#e50914]'>Anime</li>
+            <li className='cursor-pointer hover:text-[#e50914]'>Games</li>
+            <li className='cursor-pointer hover:text-[#e50914]'>New & Popular</li>
+            <li className='cursor-pointer hover:text-[#e50914]'>Upcoming</li>
         </ul>
 
-        <div>
-            <div>
-                <input type="text" placeholder='Search...'/>
+        <div className='flex items-center gap-4 relative'>
+            <div className='relative hidden md:inline-flex'>
+                <input type="text" placeholder='Search...' className='bg-[#333333] px-4 py-2
+                rounded-full min-w-72 pr-10 outline-none'/>
                 search-icon
             </div>
 
