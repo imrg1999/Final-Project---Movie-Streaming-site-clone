9df35e902def1e7b5484dcf3daeb7ee109402f36
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-12-07 01:34:01 +0530

    static data created for cards

--- movie/src/Components/Cardlist.jsx
+++ movie/src/Components/Cardlist.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
 const Cardlist = () => {
+  const data = [
+    {
+      id: 1,
+      title: "card 1",
+      description: "description for card 1",
+      imageUrl: "image"
+    }, 
+    {
+      id: 2,
+      title: "card 2",
+      description: "description for card 2",
+      imageUrl: "image 2"
+    }
+  ]
   return (
     <div>Cardlist</div>
   )
